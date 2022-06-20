b71d4fb704af34583040dc04aaafa777df3b5d21
natruz <88256962+natruz@users.noreply.github.com>
2022-06-20 09:25:58 +0800

    Initial Commit

---
+++ fathersDay2022/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  fathersDay2022
+//
+//  Created by NATALIE CHEN HUI REGINA RUZSICSK stu on 20/6/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ fathersDay2022/fathersDay2022App.swift
@@ -0,0 +1,17 @@
+//
+//  fathersDay2022App.swift
+//  fathersDay2022
+//
+//  Created by NATALIE CHEN HUI REGINA RUZSICSK stu on 20/6/22.
+//
+
+import SwiftUI
+
+@main
+struct fathersDay2022App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
